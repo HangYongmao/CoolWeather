6b2f3e71e8434f11e7c6d68a02f49a67c56eb1d6
HangYongmao <1981217256@qq.com>
2018-05-12 20:00:56 +0800

    增加清除缓存功能

--- app/src/main/java/cn/edu/sicnu/coolweather/SettingActivity.java
+++ app/src/main/java/cn/edu/sicnu/coolweather/SettingActivity.java
@@ -1,29 +1,41 @@
 package cn.edu.sicnu.coolweather;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class SettingActivity extends AppCompatActivity {
+
+    private TextView tv_cache;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,6 +111,32 @@ public class SettingActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // 计算缓存大小
+        tv_cache = findViewById(R.id.cache_size);
+        //获得应用内部缓存(/data/data/com.example.androidclearcache/cache)
+        File file = new File(this.getCacheDir().getPath());
+        try {
+            tv_cache.setText(DataCleanManager.getCacheSize(file));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 清除缓存
+        View clearCache = findViewById(R.id.setting_cache);
+        clearCache.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "清理中...", Toast.LENGTH_SHORT).show();
+                try {
+                    DataCleanManager.cleanInternalCache(getApplicationContext());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(view.getContext(), "清理完成", Toast.LENGTH_SHORT);
+                tv_cache.setText("0.0KB");
+            }
+        });
     }
 
     public boolean isNetworkConnected(Context context) {
@@ -124,16 +162,16 @@ public class SettingActivity extends AppCompatActivity {
         // 获取当前系统语言
         TextView languageText = findViewById(R.id.language);
         Locale locale = Locale.getDefault();
-        languageText.setText(locale.getLanguage() + "-" + locale.getCountry());
+        languageText.setText(locale.getLanguage() + "-" + locale.getCountry() + "＞");
     }
 
     public void getSystemNetworkStatus() {
         // 网络状态
         TextView network_status = findViewById(R.id.network_status);
         if (isNetworkConnected(network_status.getContext()) == true) {
-            network_status.setText("网络可用");
+            network_status.setText("网络可用 ＞");
         } else {
-            network_status.setText("网络不可用");
+            network_status.setText("网络不可用 ＞");
         }
     }
 
@@ -143,6 +181,6 @@ public class SettingActivity extends AppCompatActivity {
         long time = System.currentTimeMillis();
         Date date = new Date(time);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd EEEE");
-        date_now.setText(format.format(date));
+        date_now.setText(format.format(date) + " ＞");
     }
 }
