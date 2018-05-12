2ba39f956027120343d9b9e5e28902c9dde5a313
HangYongmao <1981217256@qq.com>
2018-05-12 12:39:59 +0800

    可设置语言和网络状态

--- app/src/main/java/cn/edu/sicnu/coolweather/SettingActivity.java
+++ app/src/main/java/cn/edu/sicnu/coolweather/SettingActivity.java
@@ -1,11 +1,15 @@
 package cn.edu.sicnu.coolweather;
 
+import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -24,6 +28,34 @@ public class SettingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        View about = findViewById(R.id.setting_about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(view.getContext(), "制作人：杭永茂", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+            }
+        });
+
+        View language = findViewById(R.id.setting_language);
+        language.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+
+        View network = findViewById(R.id.setting_network);
+        network.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS);
+                startActivity(intent);
             }
         });
     }
