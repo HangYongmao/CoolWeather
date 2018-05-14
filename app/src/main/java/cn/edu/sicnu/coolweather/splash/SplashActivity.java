e9c3a3320a193bd872083603461b5726fd618b54
HangYongmao <1981217256@qq.com>
2018-05-14 11:23:37 +0800

    增加启动界面

---
+++ app/src/main/java/cn/edu/sicnu/coolweather/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package cn.edu.sicnu.coolweather.splash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import cn.edu.sicnu.coolweather.MainActivity;
+import cn.edu.sicnu.coolweather.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // NO Title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        // 延时
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
