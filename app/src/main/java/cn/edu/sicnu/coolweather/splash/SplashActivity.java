dd9ebaebb0e1ad682568edd0f76f60f8de8d9417
HangYongmao <1981217256@qq.com>
2018-05-14 21:16:33 +0800

    引导界面只显示一次

--- app/src/main/java/cn/edu/sicnu/coolweather/splash/SplashActivity.java
+++ app/src/main/java/cn/edu/sicnu/coolweather/splash/SplashActivity.java
@@ -1,6 +1,8 @@
 package cn.edu.sicnu.coolweather.splash;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +21,9 @@ public class SplashActivity extends AppCompatActivity {
 
     private Handler handler = new Handler();
 
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +31,8 @@ public class SplashActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+        preferences = getSharedPreferences("phone", Context.MODE_PRIVATE);
 
         // 个性化进度条
         DilatingDotsProgressBar mDilatingDotsProgressBar = (DilatingDotsProgressBar) findViewById(R.id.progress);
@@ -41,7 +48,17 @@ public class SplashActivity extends AppCompatActivity {
                 AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getDrawable();
                 animationDrawable.start();
 
-                Intent intent = new Intent(SplashActivity.this, GuideActivity.class);
+                Intent intent;
+                // 判断是不是首次登录，
+                if (preferences.getBoolean("firststart", true)) {
+                    editor = preferences.edit();
+                    // 将登录标志位设置为false，下次登录时不在显示首次登录界面
+                    editor.putBoolean("firststart", false);
+                    editor.commit();
+                    intent = new Intent(SplashActivity.this, GuideActivity.class);
+                } else {
+                    intent = new Intent(SplashActivity.this, MainActivity.class);
+                }
                 startActivity(intent);
             }
         }, 3000);
